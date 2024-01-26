0cd1eb67b53cfe09a2f891df94d8c96479e320f8
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-26 15:16:29 +0100

    add logic for adding a todo

--- script.js
+++ script.js
@@ -1,44 +1,61 @@
-const addItem = document.querySelector(".add");
-addItem.addEventListener("submit", () => {
-  let todo = document.getElementById("text");
-  let valueTodo = todo.value;
-  let author = document.getElementById("author");
-  let valueAuthor = author.value;
+const form = document.querySelector(".form");
+const defaultTodos = [
+  {
+    author: "Sofia",
+    todo: "Make lunch",
+    done: false,
+    timestamp: new Date().getTime(),
+  },
+  {
+    author: "Sofia",
+    todo: "Make a website",
+    done: false,
+    timestamp: new Date().getTime(),
+  },
+  {
+    author: "Sofia",
+    todo: "Clean the kitchen",
+    done: false,
+    timestamp: new Date().getTime(),
+  },
+];
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todo = document.querySelector(".text");
+  const author = document.querySelector(".author");
 
-  const defaultTodos = [
-    {
-      author: valueAuthor,
-      text: valueTodo,
-      done: false,
-      timestamp: new Date().getHours() + ":" + new Date().getMinutes(),
-    },
-  ];
-  console.log(defaultTodos);
-
+const newTodo = {
+    author: author.value,
+    todo: todo.value,
+    done: false,
+    timestamp: new Date().getTime(),
+  };
+ const newTodoElement = createTodoItemAsHtml(newTodo);
+  const todoItem = document.querySelector(".todos");
+  todoItem.insertAdjacentHTML("beforebegin", newTodoElement);
 });
- function createTodoItemAsHtml(todoItem) {
-   return `
-    <article class="todo-item">
+
+function createTodoItemAsHtml(todoItem) {
+  return `<article class="todo-item">
     <div class="action-icons"><span class="done">Done</span> </div>
       <div class="content">
-        <span class="text">${todoItem.text}</span>
+        <span class="text">${todoItem.todo}</span>
       </div>
       <div class="action-icons">
-        <span class="material-icons delete">Delete</span>
-        <span class="material-icons moreInfo">Info</span>
+        <span class="material-icons delete">delete</span>
+        <span class="material-icons moreInfo">info</span>
         <div class="moreInfo">
-        <span class="author">${todoItem.author}</span>
-         <span class="author">${todoItem.timestamp}</span>
+        <span class="author">author:  ${todoItem.author}</span>
+         <span class="date">date: ${todoItem.timestamp}</span>
       </div>
       </div> 
     </article>
 `;
- }
-
- const defaultTodosAsHtml = defaultTodos.map((todo) => {
-   return createTodoItemAsHtml(todo);
- });
-
- const htmlString = defaultTodosAsHtml.join("");
- const todoItems = document.querySelector(".todos");
- todoItems.innerHTML = htmlString;
+}
+const defaultTodosAsHtml = defaultTodos.map((todo) => {
+  return createTodoItemAsHtml(todo);
+});
+console.log(defaultTodosAsHtml);
+const htmlString = defaultTodosAsHtml.join(" ");
+const todoItems = document.querySelector(".todos");
+todoItems.innerHTML = htmlString;
