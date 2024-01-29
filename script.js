4b4f97bf0dd7d19c9b97c16534811a158f3a3511
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-29 12:06:13 +0100

    add logic for done toggle

--- script.js
+++ script.js
@@ -30,31 +30,42 @@ form.addEventListener("submit", (e) => {
     done: false,
     timestamp: new Date().getTime(),
   };
-  const newTodoElement = createTodoItemAsHtml(newTodo);
+  const newTodoElement = createTodoItemAsHtml(newTodo, defaultTodos.length);
   const todoItem = document.querySelector(".todos");
   todoItem.insertAdjacentHTML("beforebegin", newTodoElement);
 });
 
-function createTodoItemAsHtml(todoItem) {
+function createTodoItemAsHtml(todoItem, index) {
   return `<article class="todo-item">
-    <div class="action-icons"><span class="done">Done</span> </div>
+    <div class="action-icons"><input type="checkbox" data-index="${index}" class="done"></span> </div>
       <div class="content">
         <span class="text">${todoItem.todo}</span>
       </div>
       <div class="action-icons">
         <span class="material-icons delete">delete</span>
-         <span class="material-icons moreInfo">info</span>
+         <span class="material-icons moreInfo">info
         <span class="mInfo"> author: ${todoItem.author} date: ${todoItem.timestamp}</span>
+        </span>
           
       
       </div> 
     </article>
 `;
 }
-const defaultTodosAsHtml = defaultTodos.map((todo) => {
-  return createTodoItemAsHtml(todo);
+
+const defaultTodosAsHtml = defaultTodos.map((todo, index) => {
+  return createTodoItemAsHtml(todo, index);
 });
 console.log(defaultTodosAsHtml);
 const htmlString = defaultTodosAsHtml.join(" ");
 const todoItems = document.querySelector(".todos");
 todoItems.innerHTML = htmlString;
+
+let done = document.querySelectorAll(".done");
+
+done.forEach((checkbox, index) => {
+  checkbox.addEventListener("change", function (e) {
+    let todoIndex = e.target.getAttribute("data-index");
+    defaultTodos[todoIndex].done = e.target.checked;
+  });
+});
