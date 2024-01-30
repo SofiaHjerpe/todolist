161c0a34b4ae5c5ce4cd29856dad8867efa7e4f7
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-30 15:14:54 +0100

    add move up and move down functionality

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ function createTodoItemAsHtml(todoItem, index) {
         <span class="text">${todoItem.todo}</span>
       </div>
       <div class="action-icons">
-         <button data-index="${index}" class="btn-icon moveUp"><span class="material-icons"> arrow_upward</span></button>
-         <button data-index="${index}" class="btn-icon moveDown"><span class="material-icons"> arrow_downward</span></button>
+         <button  class="btn-icon moveUp"><span id="${index}" class="material-icons"> arrow_upward</span></button>
+         <button  class="btn-icon moveDown"><span id="${index}"class="material-icons"> arrow_downward</span></button>
          <button class="btn-icon delete"><span class="material-icons">delete</span></button>
          <span class="material-icons moreInfo">info
         <span class="mInfo"> author: ${todoItem.author} date: ${todoItem.timestamp}</span>
@@ -40,7 +40,6 @@ function createTodoItemAsHtml(todoItem, index) {
     </article>
 `;
 }
-let button = document.querySelector(".moveUp");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -58,20 +57,38 @@ form.addEventListener("submit", (e) => {
   const newTodoElement = createTodoItemAsHtml(newTodo, defaultTodos.length - 1);
   const todoItem = document.querySelector(".todos");
   todoItem.insertAdjacentHTML("beforeend", newTodoElement);
-
-  todoItem.addEventListener("change", function (e) {
-    if (e.target.matches(".done")) {
-      let todoIndex = e.target.getAttribute("data-index");
-      defaultTodos[todoIndex].done = e.target.checked;
-    }
-  });
-  todoItem.addEventListener("click", function (e) {
-    if (e.target.matches(".delete")) {
-      todos.removeChild(e.target.closest("article"));
-    }
-  });
 });
-
+const todoItem = document.querySelector(".todos");
+todoItem.addEventListener("change", function (e) {
+  if (e.target.matches(".done")) {
+    let todoIndex = e.target.id;
+    defaultTodos[todoIndex].done = e.target.checked;
+  }
+});
+const remove = document.querySelectorAll(".delete");
+todoItem.addEventListener("click", function (e) {
+  if (e.target.matches(".delete .material-icons")) {
+    todos.removeChild(e.target.closest("article"));
+  }
+});
+const moveUp = document.querySelectorAll(".moveUp");
+const moveDown = document.querySelectorAll(".moveDown");
+todoItem.addEventListener("click", (e) => {
+  if (e.target.matches(".moveUp .material-icons")) {
+    console.log(e.target);
+    const prevSibling = e.target.closest("article").previousElementSibling;
+    const clickedElement = e.target.closest("article");
+    todos.insertBefore(clickedElement, prevSibling);
+  }
+});
+todoItem.addEventListener("click", (e) => {
+  if (e.target.matches(".moveDown .material-icons")) {
+    console.log(e.target);
+    const prevSibling = e.target.closest("article").nextElementSibling;
+    const clickedElement = e.target.closest("article");
+    todos.insertBefore(prevSibling, clickedElement);
+  }
+});
 const defaultTodosAsHtml = defaultTodos.map((todo, index) => {
   return createTodoItemAsHtml(todo, index);
 });
@@ -89,17 +106,30 @@ done.forEach((checkbox, index) => {
     defaultTodos[todoIndex].done = e.target.checked;
   });
 });
-const remove = document.querySelectorAll(".delete");
+
 remove.forEach((removeItem, index) => {
   removeItem.addEventListener("click", function (e) {
     todos.removeChild(e.target.closest("article"));
   });
 });
-const moveUp = document.querySelectorAll(".moveUp");
-const moveDown = document.querySelectorAll(".moveDown");
-moveUp.forEach((up) => {
+
+moveUp.forEach((up, index) => {
   up.addEventListener("click", (e) => {
-    let index = e.target.getAttribute("data-index");
-    todos.insertBefore(moveUp[index], moveUp[index].previousElementSibling);
+    console.log(e.target);
+    let index = e.target.id;
+    let moveUpIndex = parseInt(index);
+    const prevSibling = moveUp[moveUpIndex].closest("article").previousElementSibling;
+    const clickedElement = moveUp[moveUpIndex].closest("article");
+    todos.insertBefore(clickedElement, prevSibling);
+  });
+});
+moveDown.forEach((down, index) => {
+  down.addEventListener("click", (e) => {
+    console.log(e.target);
+    let index = e.target.id;
+    let moveDownIndex = parseInt(index);
+    const prevSibling = moveDown[moveDownIndex].closest("article").nextElementSibling;
+    const clickedElement = moveDown[moveDownIndex].closest("article");
+    todos.insertBefore(clickedElement, prevSibling);
   });
 });
