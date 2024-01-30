28e66ec0337770904b5b287a4f7d8cbf65921f6e
Sofia Hjerpe <hjerpesofia@gmail.com>
2024-01-30 12:16:04 +0100

    way to remove items from dom, add buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const form = document.querySelector(".form");
+const todos = document.querySelector(".todos");
 const defaultTodos = [
   {
     author: "Sofia",
@@ -21,13 +22,15 @@ const defaultTodos = [
 ];
 function createTodoItemAsHtml(todoItem, index) {
   return `<article class="todo-item">
-    <div class="action-icons"><input type="checkbox" data-index="${index}" class="done"
+    <div class="action-icons"><input type="checkbox" id="${index}" class="done"
      ${todoItem.done ? "checked" : ""}></input></div>
       <div class="content">
         <span class="text">${todoItem.todo}</span>
       </div>
       <div class="action-icons">
-        <span class="material-icons delete">delete</span>
+         <button data-index="${index}" class="btn-icon moveUp"><span class="material-icons"> arrow_upward</span></button>
+         <button data-index="${index}" class="btn-icon moveDown"><span class="material-icons"> arrow_downward</span></button>
+         <button class="btn-icon delete"><span class="material-icons">delete</span></button>
          <span class="material-icons moreInfo">info
         <span class="mInfo"> author: ${todoItem.author} date: ${todoItem.timestamp}</span>
         </span>
@@ -37,6 +40,7 @@ function createTodoItemAsHtml(todoItem, index) {
     </article>
 `;
 }
+let button = document.querySelector(".moveUp");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -61,8 +65,12 @@ form.addEventListener("submit", (e) => {
       defaultTodos[todoIndex].done = e.target.checked;
     }
   });
+  todoItem.addEventListener("click", function (e) {
+    if (e.target.matches(".delete")) {
+      todos.removeChild(e.target.closest("article"));
+    }
+  });
 });
-  
 
 const defaultTodosAsHtml = defaultTodos.map((todo, index) => {
   return createTodoItemAsHtml(todo, index);
@@ -77,7 +85,21 @@ let done = document.querySelectorAll(".done");
 
 done.forEach((checkbox, index) => {
   checkbox.addEventListener("change", function (e) {
-    let todoIndex = e.target.getAttribute("data-index");
+    let todoIndex = e.target.id;
     defaultTodos[todoIndex].done = e.target.checked;
+  });
+});
+const remove = document.querySelectorAll(".delete");
+remove.forEach((removeItem, index) => {
+  removeItem.addEventListener("click", function (e) {
+    todos.removeChild(e.target.closest("article"));
+  });
+});
+const moveUp = document.querySelectorAll(".moveUp");
+const moveDown = document.querySelectorAll(".moveDown");
+moveUp.forEach((up) => {
+  up.addEventListener("click", (e) => {
+    let index = e.target.getAttribute("data-index");
+    todos.insertBefore(moveUp[index], moveUp[index].previousElementSibling);
   });
 });
